d853bed5e18ed9c5542742ae4603c9da64ae76f7
Dominic Marcello <dmarce504@gmail.com>
2020-10-09 07:43:30 -0500

    Working on efficiency

--- src/gravity_queue.cpp
+++ src/gravity_queue.cpp
@@ -77,61 +77,65 @@ void gravity_queue_add_work(std::uint64_t id, std::shared_ptr<std::vector<_4forc
 	entry->units.push_back(std::move(subunit));
 	assert(entry->members_in <= entry->member_count);
 	if (entry->members_in == entry->member_count) {
-		auto func = hpx::async([entry, do_phi]() {
-			auto unit = std::move(*entry);
-			std::unordered_set<tree_ptr, tree_ptr_hash> part_requests;
-			for (int i = 0; i < unit.units.size(); i++) {
-				for (auto &this_y : unit.units[i].y) {
-					part_requests.insert(this_y);
-				}
+//		auto func = hpx::async([entry, do_phi]() {
+		auto unit = std::move(*entry);
+		std::unordered_set<tree_ptr, tree_ptr_hash> part_requests;
+		for (int i = 0; i < unit.units.size(); i++) {
+			for (auto &this_y : unit.units[i].y) {
+				part_requests.insert(this_y);
 			}
-			std::vector<tree_ptr> part_req_vec(part_requests.begin(), part_requests.end());
-			auto pos = get_positions(part_req_vec);
-			pinned_vector < part_pos > y;
-			std::vector<std::pair<int, int>> yiter;
-			std::unordered_map<tree_ptr, std::pair<int, int>, tree_ptr_hash> ymap;
-			int index = 0;
-			int i = 0;
-			for (const auto &this_pos : pos) {
-				for (const auto &p : *this_pos) {
-					y.push_back(p);
-				}
-				ymap[part_req_vec[i]] = std::make_pair(index, index + this_pos->size());
-				index += this_pos->size();
-				i++;
+		}
+		std::vector<tree_ptr> part_req_vec(part_requests.begin(), part_requests.end());
+		auto pos = get_positions(part_req_vec);
+		pinned_vector<part_pos> y;
+		y.reserve(opts.bucket_size * pos.size());
+		std::vector<std::pair<int, int>> yiter;
+		static thread_local std::unordered_map<tree_ptr, std::pair<int, int>, tree_ptr_hash> ymap;
+		ymap.clear();
+		int index = 0;
+		int i = 0;
+		for (const auto &this_pos : pos) {
+			for (const auto &p : *this_pos) {
+				y.push_back(p);
 			}
-			std::vector<cuda_work_unit> gunits;
-			for (auto &u : unit.units) {
-				cuda_work_unit this_subunit;
-				this_subunit.fptr = u.f;
-				this_subunit.xptr = u.x;
-				this_subunit.z = std::move(u.z);
-				std::vector<std::pair<int, int>> these_iters;
-				for (int j = 0; j < u.y.size(); j++) {
-					these_iters.push_back(ymap[u.y[j]]);
-				}
-				for (auto &iter : these_iters) {
-					while (iter.first != iter.second) {
-						std::pair<int, int> this_iter;
-						this_iter.first = iter.first;
-						this_iter.second = std::min(iter.second, this_iter.first + SYNCRATE);
-						iter.first = this_iter.second;
-						this_subunit.yiters.push_back(this_iter);
-					}
-
-				}
-				gunits.push_back(std::move(this_subunit));
+			ymap[part_req_vec[i]] = std::make_pair(index, index + this_pos->size());
+			index += this_pos->size();
+			i++;
+		}
+		std::vector<cuda_work_unit> gunits;
+		for (auto &u : unit.units) {
+			cuda_work_unit this_subunit;
+			this_subunit.fptr = u.f;
+			this_subunit.xptr = u.x;
+			this_subunit.z = std::move(u.z);
+			std::vector<std::pair<int, int>> these_iters;
+			these_iters.reserve(u.y.size());
+			for (int j = 0; j < u.y.size(); j++) {
+				these_iters.push_back(ymap[u.y[j]]);
 			}
+			for (auto &iter : these_iters) {
+				while (iter.first != iter.second) {
+					std::pair<int, int> this_iter;
+					this_iter.first = iter.first;
+					this_iter.second = std::min(iter.second, this_iter.first + SYNCRATE);
+					iter.first = this_iter.second;
+					this_subunit.yiters.push_back(this_iter);
+				}
 
-			gravity_PP_direct_cuda(std::move(gunits), y, do_phi);
-
-			for (auto &u : unit.units) {
-				u.callback();
 			}
+			gunits.push_back(std::move(this_subunit));
+		}
 
-		});
-		std::lock_guard < mutex_type > lock(fut_mtx);
-		futures.push_back(std::move(func));
+		gravity_PP_direct_cuda(std::move(gunits), y, do_phi);
+
+		for (auto &u : unit.units) {
+			u.callback();
+		}
+
+//		});
+//		func.get();
+//		std::lock_guard < mutex_type > lock(fut_mtx);
+//		futures.push_back(std::move(func));
 	}
 }
 
