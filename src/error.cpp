3de9d352d02a63355a6dcd8506382f1b2675f34e
Dominic Marcello <dmarce504@gmail.com>
2020-10-11 10:15:08 -0500

    Working?

--- src/error.cpp
+++ src/error.cpp
@@ -19,17 +19,17 @@ std::pair<double, double> compute_error(std::vector<output_part> &parts) {
 	std::vector<vect<double>> y;
 	std::vector<int> indices;
 	const int sample_size = std::min(SAMPLE_SIZE, (int) parts.size());
-//	if (sample_size == SAMPLE_SIZE) {
-//		std::unordered_set<int> i1;
-//		while (i1.size() < sample_size) {
-//			i1.insert(rand() % parts.size());
-//		}
-//		indices.insert(indices.begin(), i1.begin(), i1.end());
-//	} else {
+	if (sample_size == SAMPLE_SIZE) {
+		std::unordered_set<int> i1;
+		while (i1.size() < sample_size) {
+			i1.insert(rand() % parts.size());
+		}
+		indices.insert(indices.begin(), i1.begin(), i1.end());
+	} else {
 		for (int i = 0; i < parts.size(); i++) {
 			indices.push_back(i);
 		}
-//	}
+	}
 
 	for (int i = 0; i < indices.size(); i++) {
 		x.push_back(parts[indices[i]].x);
